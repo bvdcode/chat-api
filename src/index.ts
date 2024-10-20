deea31511d631ba47ab46a553664a63adaa5a07f
Vadim Belov <github@belov.us>
2024-10-20 16:54:51 -0700

    Refactor fetch function to handle specific API endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
 		const url = new URL(request.url);
-		if (url.pathname === '/api/v1/my-address') {
+		if (url.pathname === '/my-address') {
 			const ip = request.headers.get('cf-connecting-ip');
 			return new Response(`Hello from Cloudflare Worker! Your IP is ${ip}`, {
 				headers: { 'content-type': 'text/plain' },
