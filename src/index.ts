9e12e0298bb40f054f1a196691c59ad3bdcc4762
Vadim Belov <github@belov.us>
2024-10-23 15:19:59 -0700

    Refactor fetch function to include CORS headers in response

--- src/index.ts
+++ src/index.ts
@@ -10,51 +10,59 @@ export default {
 		const keyValuePath = '/key-value/';
 		const valueSizeLimit = 2 * 1024 * 1024; // 2 MB
 
+		const corsHeaders = {
+			'Access-Control-Allow-Origin': 'http://localhost:5173, https://diary.belov.us',
+			'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+			'Access-Control-Allow-Headers': 'Content-Type',
+		};
+
+		if (request.method === 'OPTIONS') {
+			return new Response(null, { headers: corsHeaders });
+		}
+
 		if (url.pathname.startsWith(keyValuePath) && request.method === 'POST') {
 			const key = url.pathname.slice(keyValuePath.length).trim();
 			const value = await request.text();
 			if (!value) {
-				return new Response('No value provided', { status: 400 });
+				return new Response('No value provided', { status: 400, headers: corsHeaders });
 			}
 			if (key.length > 255) {
-				return new Response('Key too long', { status: 400 });
+				return new Response('Key too long', { status: 400, headers: corsHeaders });
 			}
 			if (value.length > valueSizeLimit) {
-				return new Response('Value too long', { status: 400 });
+				return new Response('Value too long', { status: 400, headers: corsHeaders });
 			}
 			if (!key) {
-				return new Response('No key provided', { status: 400 });
+				return new Response('No key provided', { status: 400, headers: corsHeaders });
 			}
 			const userAgent = request.headers.get('user-agent');
 			if (!userAgent) {
-				return new Response('No user-agent header', { status: 400 });
+				return new Response('No user-agent header', { status: 400, headers: corsHeaders });
 			}
 			const ipAddr = request.headers.get('cf-connecting-ip');
 			if (!ipAddr) {
-				return new Response('No IP address header', { status: 400 });
+				return new Response('No IP address header', { status: 400, headers: corsHeaders });
 			}
 
-			// 'INSERT INTO user_key_values (key, value, user_agent, ip_address) VALUES (?, ?, ?, ?)'
-			// вставить значения если их нет, иначе обновить
 			const query = 'INSERT OR REPLACE INTO user_key_values (key, value, user_agent, ip_address) VALUES (?, ?, ?, ?)';
 
 			await env.DB.prepare(query).bind(key, value, userAgent, ipAddr).run();
-			return new Response('OK');
+			return new Response('OK', { headers: corsHeaders });
 		}
 
 		if (url.pathname.startsWith(keyValuePath) && request.method === 'GET') {
 			const key = url.pathname.slice(keyValuePath.length).trim();
 			if (!key) {
-				return new Response('No key provided', { status: 400 });
+				return new Response('No key provided', { status: 400, headers: corsHeaders });
 			}
 			const row = await env.DB.prepare('SELECT value, created_at FROM user_key_values WHERE key = ?').bind(key).first();
 			if (!row) {
-				return new Response('Not Found', { status: 404 });
+				return new Response('Not Found', { status: 404, headers: corsHeaders });
 			}
 
-			// got value and created_at, return value and add created_at to response headers
 			return new Response(String(row.value), {
 				headers: {
+					...corsHeaders,
 					'Content-Type': 'text/plain; charset=utf-8',
 					'Last-Modified': new Date(row.created_at as string).toUTCString(),
 					'X-Created-At': new Date(row.created_at as string).toISOString(),
@@ -65,14 +73,14 @@ export default {
 		if (url.pathname === '/my-address') {
 			const ip = request.headers.get('cf-connecting-ip');
 			return new Response(`Hello from Cloudflare Worker! Your IP is ${ip}`, {
-				headers: { 'content-type': 'text/plain' },
+				headers: { ...corsHeaders, 'content-type': 'text/plain' },
 			});
 		}
 
 		if (url.pathname === '/ping') {
-			return new Response('Pong', { headers: { 'content-type': 'text/plain' } });
+			return new Response('Pong', { headers: { ...corsHeaders, 'content-type': 'text/plain' } });
 		}
 
-		return new Response('Not Found', { status: 404 });
+		return new Response('Not Found', { status: 404, headers: corsHeaders });
 	},
 } satisfies ExportedHandler<Env>;
